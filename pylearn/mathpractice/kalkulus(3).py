013f81610bd17423d24f057239dbc3b096cb609c
Qin <mutaqinsyahril07@gmail.com>
2026-05-24 18:49:44 +0700

    kalkulus(3)-logaritmik & trigonometrik

---
+++ pylearn/mathpractice/kalkulus(3).py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# kalkulus 
+
+# fungsi logaritmik 
+
+x = np.linspace(0.1, 10, 400)
+y = np.log(x)
+
+plt.plot(x, y, label='f(x) = log(x)')
+plt.title("Logarithmic Function")
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.legend()
+plt.show()
+
+# fungsi trigonometrik
+
+x = np.linspace(-10, 10, 400)
+y = np.sin(x)
+
+plt.plot(x, y, label='f(x) = sin(x)')
+plt.title("Trigonometric Function")
+plt.xlabel('x')
+plt.ylabel('f(x)')  
+plt.legend()
+plt.show()
